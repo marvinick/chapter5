5db239cecc046a9bca88142f47799acaa6600823
Marvki <marvkipi@gmail.com>
2016-03-24 08:15:18 -0700

    tabs and navigation

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,15 @@
 angular.module('App', ['ionic'])
 
+angular.module('App', ['ionic'])
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('tabs', {
+				url: '/tabs',
+				templateUrl: 'views/tabs/tabs.html'
+			})
+		$urlRouterProvider.otherwise('/tabs');
+	})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
