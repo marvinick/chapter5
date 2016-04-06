777ae1773d119eb13a6999a82f14d0c2c2ec3c92
Marvki <marvkipi@gmail.com>
2016-04-05 23:34:00 -0700

    history.js update

--- www/js/app.js
+++ www/js/app.js
@@ -17,10 +17,11 @@ angular.module('App', ['ionic', 'highcharts-ng'])
 			}
 		})
 		.state('tabs.history', {
-			url: '/history',
+			url: '/history?currency',
 			views: {
 				'history-tab': {
-					templateUrl: 'views/history/history.html'
+					templateUrl: 'views/history/history.html',
+					controller: "HistoryController"
 				}
 			}
 		})
